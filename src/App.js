5a9c77f6301c16994bbcd18b337e80f354e73559
JoBergstrom <katrinamack254@gmail.com>
2022-10-19 20:55:32 -0500

    MyLibbb/005/Dependencies_Update_02
    - Updated node modules and yarn packages.
    - Organized React component code using linter tools

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import "./App.css";
-import React from "react";
-import { AiFillQuestionCircle } from "react-icons/ai";
-import { GiMagnifyingGlass } from "react-icons/gi";
-import { BsFillBookmarksFill } from "react-icons/bs";
-import { HiDocumentText } from "react-icons/hi";
+import './App.css'
+import React from 'react'
+import { AiFillQuestionCircle } from 'react-icons/ai'
+import { GiMagnifyingGlass } from 'react-icons/gi'
+import { BsFillBookmarksFill } from 'react-icons/bs'
+import { HiDocumentText } from 'react-icons/hi'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="headerStyle">
-          <a href={""}>
-            <li className="menuItem">
-              <GiMagnifyingGlass /> Search
-            </li>
-          </a>{" "}
-          <a href={""}>
-            <li className="menuItem">
-              <BsFillBookmarksFill /> My library
-            </li>
-          </a>{" "}
-          <li className="menuItem">
-            <AiFillQuestionCircle /> About
-          </li>
-          <li className="menuItem">
-            <HiDocumentText /> Docs
-          </li>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="headerStyle">
+                    <a href={''}>
+                        <li className="menuItem">
+                            <GiMagnifyingGlass /> Search
+                        </li>
+                    </a>{' '}
+                    <a href={''}>
+                        <li className="menuItem">
+                            <BsFillBookmarksFill /> My library
+                        </li>
+                    </a>{' '}
+                    <li className="menuItem">
+                        <AiFillQuestionCircle /> About
+                    </li>
+                    <li className="menuItem">
+                        <HiDocumentText /> Docs
+                    </li>
+                </header>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
